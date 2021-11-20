05efe806a4503a7adbd60711b0c6f7ecfd696dd6
Niraj-123456 <lamaniraj321@gmail.com>
2021-11-20 12:49:19 +0545

    Filter with genre added

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { getMovies } from "../fakeMovieService";
+import { getGenres } from "../fakeGenreService";
 import Like from "./common/like";
 
 class Movie extends Component {
   state = {
     movies: getMovies(),
+    genres: getGenres(),
   };
 
   handleDelete = (movie) => {
@@ -23,45 +25,64 @@ class Movie extends Component {
     const count = this.state.movies.length;
     return (
       <div className="container my-3">
-        <h3>There are {count} movies in database.</h3>
-        <table className="table my-3">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rating</th>
-              <th>Like</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.movies.map((movie) => {
-              return (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like
-                      onLiked={() => this.handleLike(movie)}
-                      liked={movie.liked}
-                    />
-                  </td>
-                  <td>
-                    <a
-                      onClick={() => this.handleDelete(movie)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      Delete
-                    </a>
-                  </td>
+        <div className="row">
+          <div className="col">
+            <h3 className="text-center">
+              There are {count} movies in database.
+            </h3>
+          </div>
+        </div>
+        <div className="row my-3">
+          <div className="col-3 my-2">
+            <ul className="list-group">
+              {this.state.genres.map((genre) => (
+                <li key={genre._id} className="list-group-item">
+                  {genre.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Genre</th>
+                  <th>Stock</th>
+                  <th>Rating</th>
+                  <th>Like</th>
+                  <th>Action</th>
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
+              </thead>
+              <tbody>
+                {this.state.movies.map((movie) => {
+                  return (
+                    <tr key={movie._id}>
+                      <td>{movie.title}</td>
+                      <td>{movie.genre.name}</td>
+                      <td>{movie.numberInStock}</td>
+                      <td>{movie.dailyRentalRate}</td>
+                      <td>
+                        <Like
+                          onLiked={() => this.handleLike(movie)}
+                          liked={movie.liked}
+                        />
+                      </td>
+                      <td>
+                        <a
+                          onClick={() => this.handleDelete(movie)}
+                          className="btn btn-danger btn-sm"
+                        >
+                          Delete
+                        </a>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
